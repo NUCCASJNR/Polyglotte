989bd48287af6a6366263640c163039c703023d9
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-18 15:50:19 +0100

    Added a new route to ser.py to retrieve a particular user using the user_id

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -8,11 +8,27 @@ from models.user import User
 
 @app_views.route("/users", methods=["GET"], strict_slashes=False)
 def get_users():
+    """
+    Docs later
+    """
+
     users_list = []
     users = storage.all(User)
     for user in users.values():
         users_list.append(user.to_dict())
     return jsonify(users_list)
+
+
+@app_views.route("/users/<user_id>", methods=["GET"], strict_slashes=False)
+def get_user_using_userid(user_id):
+    """
+    Gets user uring the user_id
+    """
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    abort(404)
+
 
 @app_views.route("/users", methods=["POST"], strict_slashes=False)
 def post_user():
@@ -25,6 +41,4 @@ def post_user():
     for key, value in user_data.items():
         setattr(user, key, value)
     user.save()
-    storage.new(user)
-    storage.save()
     return jsonify(user.to_dict()), 201
