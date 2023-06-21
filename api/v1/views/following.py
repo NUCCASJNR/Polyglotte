49b05f7e424c29b82f8cc12f279f51767314b8b3
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-21 16:50:18 +0100

    following route

--- api/v1/views/following.py
+++ api/v1/views/following.py
@@ -1,63 +1,37 @@
-# #!/usr/bin/python3
-# """
-# HAndles all RESTFUL APIs for following
-# """
+#!/usr/bin/python3
+"""
+HAndles all RESTFUL APIs for following
+"""
 
-# from flask import abort, jsonify, request
+from flask import abort, jsonify, request
 
-# from api.v1.views import app_views
-# from models import storage
-# from models.follower import Follower
-# from models.user import User
+from api.v1.views import app_views
+from models import storage
+from models.following import Following
+from models.user import User
 
 
-# @app_views.route("/users/<user_id>/follow", methods=["POST"],
-#                  strict_slashes=False)
-# def post_new_follower(user_id):
-#     """
-#     Adds a user as a follower to a user
-#     """
-#     follower_data = request.get_json()
-#     follower_id = follower_data.get("followed_user_id")
-#     follower = storage.get(Follower, follower_id)
-#     print(follower)
-#     if not follower:
-#         new_user = storage.get(User, user_id)
-#         print(new_user)
-#         if not follower_data:
-#             return jsonify({"error": "Not a JSON"})
-#         if follower_id  == user_id:
-#             return jsonify({"error": "Cannot follow yourself"})
-#         follow = Follower()
-#         for key, value in follower_data.items():
-#             setattr(follow, key, value)
-#         new_user.increment_followers_count()
-#         follow.save()
-#         return jsonify(follow.to_dict()), 201
+@app_views.route("/users/<user_id>/following", methods=["POST"],
+                 strict_slashes=False)
+def following(user_id):
+    """
+    Adds a user as a follower to a user
+    """
+    follower_data = request.get_json()
+    following_id = follower_data.get("follower_user_id")
+    following = storage.get(Following, following_id)
+    if not following:
+        new_user = storage.get(User, user_id)
+        print(new_user)
+        if not follower_data:
+            return jsonify({"error": "Not a JSON"})
+        if following_id  == user_id:
+            return jsonify({"error": "Cannot follow yourself"})
+        follow = Following()
+        for key, value in follower_data.items():
+            setattr(follow, key, value)
+        new_user.increment_following_count()
+        follow.save()
+        return jsonify(follow.to_dict()), 201
     
-#     abort(404)
-
-# @app_views.route("/users/<user_id>/unfollow", methods=["POST"],
-#                  strict_slashes=False)
-# def unfollow(user_id):
-#     """
-#     unfollows a user as a follower to a user
-#     """
-#     follower_data = request.get_json()
-#     follower_id = follower_data.get("followed_user_id")
-#     follower = storage.get(Follower, follower_id)
-#     print(follower)
-#     if not follower:
-#         new_user = storage.get(User, user_id)
-#         print(new_user)
-#         if not follower_data:
-#             return jsonify({"error": "Not a JSON"})
-#         if follower_id  == user_id:
-#             return jsonify({"error": "Cannot unfollow yourself"})
-#         follow = Follower()
-#         for key, value in follower_data.items():
-#             setattr(follow, key, value)
-#         new_user.decrement_followers_count()
-#         follow.save()
-#         return jsonify(follow.to_dict()), 201
-#     abort(404)
+    abort(404)
