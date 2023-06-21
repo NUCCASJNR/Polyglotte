7ace80ce460ef1d68fc3b88099dc896b05a8dada
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-21 01:47:42 +0100

    Added a new route to hanldle followers tho it still needs to be updated to fit in project requirement

--- api/v1/views/follower.py
+++ api/v1/views/follower.py
@@ -11,6 +11,22 @@ from models.follower import Follower
 from models.user import User
 
 
-# @app_views.route("/users/<user_id>/follow", methods=["POST"],
-#                  strict_slashes=False)
-# def post_new_follower
+@app_views.route("/users/<user_id>/follow", methods=["POST"],
+                 strict_slashes=False)
+def post_new_follower(user_id):
+    """
+    Adds a user as a follower to a user
+    """
+    follower_data = request.get_json()
+    new_user = storage.get(User, user_id) # Get the existing user object
+    if not follower_data:
+        return jsonify({"error": "Not a JSON"})
+    if new_user == follower_data.get("user_id"):
+        return jsonify({"error": "Cannot follow yourself"})
+    follow = Follower()
+    for key, value in follower_data.items():
+        setattr(follow, key, value)
+    new_user.increment_followers_count()
+    follow.save()
+    return jsonify(follow.to_dict()), 201
+    
