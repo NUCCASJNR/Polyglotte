7c6e4cd0b08dff618825844b0612bc735473c014
Frankenstein9585 <donny9585@gmail.com>
2023-07-10 17:47:55 +0100

    Added Translation Functionality

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
-# #!/usr/bin/python3
+from google.cloud import translate_v2 as translate
 import os
-from os import getenv
-from google.cloud.translate_v2.client import Client
 
-translate_client = Client()
 
-string = 'Hello World'
+# os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
-translation = translate_client.translate(string, target_language='ja')
+def translate_text(target: str, text: str) -> dict:
+    """Translates text into the target language.
 
-print(translation)
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+
+    translate_client = translate.Client()
+
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    print("Text: {}".format(result["input"]))
+    print("Translation: {}".format(result["translatedText"]))
+    print("Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+    return result
+
+
+translate_text('en', 'Bonjour monsieur')
