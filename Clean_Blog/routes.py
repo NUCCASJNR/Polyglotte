818bdf9ba1d8f8ae2b47b927561204ff4e3cadba
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-17 10:12:35 +0100

    Email verification handler

--- Clean_Blog/routes.py
+++ Clean_Blog/routes.py
@@ -103,6 +103,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        if user.verified == 0:
+            flash('You need to verify your account before you can login')
+            return redirect(url_for('index'))
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
